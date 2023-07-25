64015b26dc063c086a5608326eee4d4d5a81d043
Luis <rodriguesluis414@gmail.com>
2023-07-25 01:04:28 -0500

    Agregue movimiendo la jugador, y le dimos escucha a los botones

--- src/game.js
+++ src/game.js
@@ -1,17 +1,27 @@
 const canvas = document.querySelector("#game")
 const game = canvas.getContext("2d")
+const btnUp = document.querySelector("#up")
+const btnLeft = document.querySelector("#left")
+const btnRight = document.querySelector("#right")
+const btnDown = document.querySelector("#down")
 
-let canvasSize
-let elementsSize
+let canvasSize //Es variable el tama;a del cambas
+let elementsSize // Es variable el tama;no del elemento
+
+const playerPosition = {
+  //Creamos la posicion del jugador
+  x: undefined,
+  y: undefined,
+}
 
 window.addEventListener("load", setCanvasSize) //Escuchar apenas cargue el archivo HTML
 window.addEventListener("resize", setCanvasSize) //escuchamos el reize, que lo que hace es que cada vez que se cambie el tama;o de la pantalla llama a la function Stargame
 
 function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
-    canvasSize = window.innerWidth * 0.85
+    canvasSize = window.innerWidth * 0.8
   } else {
-    canvasSize = window.innerHeight * 0.85
+    canvasSize = window.innerHeight * 0.8
   }
   canvas.setAttribute("width", canvasSize)
   canvas.setAttribute("height", canvasSize)
@@ -22,47 +32,99 @@ function setCanvasSize() {
 }
 
 function startGame() {
-  let canvasSize
-
   console.log({ canvasSize, elementsSize })
 
   game.font = elementsSize + "px Verdana"
   game.textAlign = "end"
 
   const map = maps[0]
-  const mapRows = map.trim().split("\n")
+  const mapRows = map.trim().split("\n") //usamos .trim para limpiarlo y con .split le quitamos los saltos de linea
   const mapRowCols = mapRows.map((row) => row.trim().split(""))
   console.log({ map, mapRows, mapRowCols })
 
-  for (let row = 1; row <= 10; row++) {
-    for (let col = 1; col <= 10; col++) {
-      game.fillText(
-        emojis[mapRowCols[row - 1][col - 1]],
-        elementsSize * col,
-        elementsSize * row
-      )
-    }
-  }
+  game.clearRect(0, 0, canvasSize, canvasSize)
+  mapRowCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col]
+      const posy = elementsSize * (rowI + 1)
+      const posx = elementsSize * (colI + 1)
 
-  //   game.fillText(emojis["X"], 100, 100)
-  //   game.fillText(emojis["X"], 100, 100)
-  //   game.fillText(emojis["X"], 100, 100)
-  //   game.fillText(emojis["X"], 100, 100)
-  //   game.fillText(emojis["X"], 100, 100)
-  //   game.fillText(emojis["X"], 100, 100)
-  //   game.fillText(emojis["X"], 100, 100)
-  //   game.fillText(emojis["X"], 100, 100)
-  //   game.fillText(emojis["X"], 100, 100)
-  //   game.fillText(emojis["X"], 100, 100)
+      if (col == "O") {
+        // si  la columna es el emogi
+        if (!playerPosition.x && !playerPosition.y) {
+          playerPosition.x = posx // la pedimos la posicion en X
+          playerPosition.y = posy // y tambien en Y
+          console.log({ playerPosition })
+        }
+      }
 
-  //window.innerHeight
-  //window.innerWidth
-
-  //game.fillRect(0, 50, 100, 100) //Definir donde va iniciar el trazo
-  //game.clearRect(50, 50, 50, 50) //borramos el rectangulo de dichas medidas
-  //   game.clearRect(0, 0, 50, 50) // (50(horizontal), 50,(vertical), 50(Ancho), 50(alto))
-  //   game.font = "13px verdana" //Agregamos el tamano del texto y la fuente
-  //   game.fillStyle = "black" //le asignamos el color que va tener el texto
-  //   game.textAlign = "end"
-  //   game.fillText("Luis", 25, 25) //agregamos un nombre como ejemplo, y le damos las cordenadas de ubicacion
+      game.fillText(emoji, posx, posy)
+    })
+  })
+  movePlayer() // hay que llamar la funcion de movimiento
 }
+
+function movePlayer() {
+  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y) // le inertamos el emoji a la posicion en X & Y
+}
+
+window.addEventListener("keydown", moveByKeys)
+//Escuchar Movimientos
+btnUp.addEventListener("click", moveUp)
+btnLeft.addEventListener("click", moveLeft)
+btnRight.addEventListener("click", moveRight)
+btnDown.addEventListener("click", moveDown)
+//Funcion para mover dependiendo de la letra seleccionada
+function moveByKeys(event) {
+  if (event.key === "ArrowUp") moveUp() //escuchamos el evento de subir
+  else if (event.key === "ArrowLeft") moveLeft() //izquierda
+  else if (event.key === "ArrowRight") moveRight() //derecha
+  else if (event.key === "ArrowDown") moveDown() //Abajo
+}
+
+function moveUp() {
+  //Mover Arriba
+  console.log("me quieron mover hacia arriba")
+  playerPosition.y -= elementsSize // la posicon de Y va ser el valor de elementSize
+  startGame()
+}
+function moveLeft() {
+  //Mover a la Izquierda
+  playerPosition.x -= elementsSize // la posicon de x va ser el valor de elementSize
+  startGame()
+  console.log("me quieron mover hacaia la izquierda")
+}
+function moveRight() {
+  //MOver a la derecha
+  playerPosition.x += elementsSize // la posicon de x va ser el valor de elementSize
+  startGame()
+  console.log("me quieron mover hacia la derecha")
+}
+function moveDown() {
+  // Mover Abajo
+  playerPosition.y += elementsSize // la posicon de Y va ser el valor de elementSize
+  startGame()
+  console.log("me quieron mover hacia abajo")
+}
+
+//   game.fillText(emojis["X"], 100, 100)
+//   game.fillText(emojis["X"], 100, 100)
+//   game.fillText(emojis["X"], 100, 100)
+//   game.fillText(emojis["X"], 100, 100)
+//   game.fillText(emojis["X"], 100, 100)
+//   game.fillText(emojis["X"], 100, 100)
+//   game.fillText(emojis["X"], 100, 100)
+//   game.fillText(emojis["X"], 100, 100)
+//   game.fillText(emojis["X"], 100, 100)
+//   game.fillText(emojis["X"], 100, 100)
+
+//window.innerHeight
+//window.innerWidth
+
+//game.fillRect(0, 50, 100, 100) //Definir donde va iniciar el trazo
+//game.clearRect(50, 50, 50, 50) //borramos el rectangulo de dichas medidas
+//   game.clearRect(0, 0, 50, 50) // (50(horizontal), 50,(vertical), 50(Ancho), 50(alto))
+//   game.font = "13px verdana" //Agregamos el tamano del texto y la fuente
+//   game.fillStyle = "black" //le asignamos el color que va tener el texto
+//   game.textAlign = "end"
+//   game.fillText("Luis", 25, 25) //agregamos un nombre como ejemplo, y le damos las cordenadas de ubicacion
